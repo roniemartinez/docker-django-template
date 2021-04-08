a35b2a7b19bba87166922b85614dba9a46b63cf9
Ronie Martinez <ronmarti18@gmail.com>
2021-04-08 14:29:10 +0800

    Revert default_urlconf (#5)

--- project/urls.py
+++ project/urls.py
@@ -17,12 +17,15 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.views import debug
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("sample/", include("sample.urls")),
     path("i18n/", include("django.conf.urls.i18n")),
 ]
+
 if settings.DEBUG:
+    urlpatterns += [path("", debug.default_urlconf)]
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
