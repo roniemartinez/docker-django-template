cbb4bf2f861c2cbb293c8e99bd24ff7e9b0688ce
Ronie Martinez <ronmarti18@gmail.com>
2021-05-13 20:55:38 +0200

    Prevent using virtualenv in docker (#18)

--- runserver.sh
+++ runserver.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ "${DEBUG}" == "true" ]]; then
-  poetry run python manage.py runserver 0.0.0.0:8000
+  python manage.py runserver 0.0.0.0:8000
 else
-  poetry run daphne -b 0.0.0.0 project.asgi:application
+  daphne -b 0.0.0.0 project.asgi:application
 fi
