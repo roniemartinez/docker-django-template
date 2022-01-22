ba87b9443cab33099f8a72f06f8af8527b17f684
Ronie Martinez <ronmarti18@gmail.com>
2022-01-22 12:56:46 +0100

    🐛 PostgreSQL host as env (#78)

--- project/settings.py
+++ project/settings.py
@@ -100,7 +100,7 @@ DATABASES = {
         "NAME": os.getenv("POSTGRES_DB"),
         "USER": os.getenv("POSTGRES_USER"),
         "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
-        "HOST": "database",
+        "HOST": os.getenv("POSTGRES_HOST"),
         "PORT": 5432,
     }
 }
