f708d977aa3754b32ba8f2275feb1339523f124b
Ronie Martinez <ronmarti18@gmail.com>
2023-12-29 01:48:31 +0000

    🐛 Fix allauth middleware

--- project/settings.py
+++ project/settings.py
@@ -62,6 +62,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "allauth.account.middleware.AccountMiddleware",
 ]
 
 ROOT_URLCONF = "project.urls"
